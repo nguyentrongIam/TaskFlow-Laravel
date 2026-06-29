1edbb63337e1ebcecaa81b505216535d743cce97
Lam777 <lam53249@gmail.com>
2026-06-29 13:58:38 +0700

    hihi

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Demo Laravel</title>
+</head>
+<body>
+    <div>
+        <h1>MSSV: 0306241377</h1>
+        <p>Tên: Nguyễn Trọng Lam</p>
+        <p>Trường: Cao đẳng Kỹ thuật Cao Thắng</p>
+        <p>Lớp: CĐ CNTT 24 WebD</p>
+    </div>
+</body>
+<style>
+    body{
+        color: pink;
+        text-align: center;
+        padding: 100px 0px 0px 0px;
+    }
+</style>
+</html>
